a5a96e082e15d2334311e76df2af2ea748871e6d
Harish Saravanan Anuja <>
2024-05-11 20:38:24 +0300

    read blob

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
+use std::io::Read;
+
+
+fn init_repo() {
+    fs::create_dir(".git").unwrap();
+    fs::create_dir(".git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+    println!("Initialized git directory")
+}
+
+fn unzip_content(sha: &str) {
+    let path = format!(".git/objects/{}/{}", &sha[..2], &sha[2..]);
+    let content = fs::read(path).unwrap();
+    let decompressed = flate2::read::ZlibDecoder::new(&content[..]);
+    let mut s = String::new();
+    std::io::BufReader::new(decompressed).read_to_string(&mut s).unwrap();
+    println!("{}", s);
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,11 +27,10 @@ fn main() {
     // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
+        init_repo();
+    } else if args[1] == "cat-file" {
+        // git cat-file -p <blob_sha>
+        unzip_content(&args[3]);
     } else {
         println!("unknown command: {}", args[1])
     }
