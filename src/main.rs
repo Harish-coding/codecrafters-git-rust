d18db5f8150711dfdd979444cb9e59cb22a9c61f
Harish Saravanan Anuja <>
2024-05-13 14:43:59 +0300

    clone repo

--- src/main.rs
+++ src/main.rs
@@ -251,7 +251,7 @@ fn create_commit(tree_sha: &str, message: &str, parent_commit_sha: Option<&str>)
     let mut commit_content_bytes = format!("commit {}\x00", commit_content.len()).into_bytes();
     commit_content_bytes.extend_from_slice(&commit_content);
     let commit_content = commit_content_bytes;
-    
+
     // hash the content
     let mut hasher = Sha1::new();
     hasher.update(commit_content.clone());
@@ -272,6 +272,86 @@ fn create_commit(tree_sha: &str, message: &str, parent_commit_sha: Option<&str>)
     println!("{}", hash);
 }
 
+// clone the repository from the url to the directory
+fn clone_repo(url: &str, dir: &str) {
+    // make directory
+    fs::create_dir_all(dir).unwrap();
+
+    // download the .git directory
+    
+
+    // implement smart http protocol for downloading the .git directory
+    let url = url.to_string() + "/.git/objects";
+    let client = reqwest::blocking::Client::new();
+    let response = client.get(&url).send().unwrap();
+    let body = response.text().unwrap();
+
+    // create the .git directory
+    fs::create_dir_all(format!("{}/.git/objects", dir)).unwrap();
+
+    // loop through the objects and download them
+    for line in body.lines() {
+        let line = line.unwrap();
+        let sha = &line[..2];
+        let file = &line[2..];
+        let url = format!("{}/{}", url, file);
+        let response = client.get(&url).send().unwrap();
+        let body = response.text().unwrap();
+        let path = format!("{}/.git/objects/{}/{}", dir, sha, file);
+        fs::create_dir_all(format!("{}/.git/objects/{}", dir, sha)).unwrap();
+        fs::write(path, body).unwrap();
+    }
+
+    // download the refs directory
+    let url = url.to_string() + "/.git/refs";
+    let response = client.get(&url).send().unwrap();
+    let body = response.text().unwrap();
+
+    // create the refs directory
+    fs::create_dir_all(format!("{}/.git/refs", dir)).unwrap();
+
+
+    // loop through the refs and download them
+    for line in body.lines() {
+        let line = line.unwrap();
+        let file = &line[2..];
+        let url = format!("{}/{}", url, file);
+        let response = client.get(&url).send().unwrap();
+        let body = response.text().unwrap();
+        let path = format!("{}/.git/refs/{}", dir, file);
+        fs::write(path, body).unwrap();
+    }
+
+    // download the HEAD file
+    let url = url.to_string() + "/.git/HEAD";
+    let response = client.get(&url).send().unwrap();
+    let body = response.text().unwrap();
+    let path = format!("{}/.git/HEAD", dir);
+    fs::write(path, body).unwrap();
+
+    // get head commit sha
+    let head = fs::read_to_string(format!("{}/.git/HEAD", dir)).unwrap();
+    let head = head.split(": ").collect::<Vec<&str>>()[1].trim();
+    let head = fs::read_to_string(format!("{}/.git/{}", dir, head)).unwrap();
+    let head = head.trim();
+
+    // create the commit file
+    let path = format!("{}/.git/{}", dir, head);
+    let content = fs::read(path).unwrap();
+    let decompressed = flate2::read::ZlibDecoder::new(&content[..]);
+    let mut s = String::new();
+    std::io::BufReader::new(decompressed).read_to_string(&mut s).unwrap();
+
+    // extract the tree sha
+    let tree_sha = s.split("\n").collect::<Vec<&str>>()[0].split(" ").collect::<Vec<&str>>()[1];
+
+    // create the tree
+    let tree_sha = create_tree(dir);
+
+    // create the checkout commit
+    create_commit(&tree_sha, "checkout", Some(head));
+
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -343,6 +423,10 @@ fn main() {
         // create the commit
         create_commit(&args[2], message, parent_commit_sha);
         
+    } else if args[1] == "clone" {
+        // git clone <url> <dir>
+        clone_repo(&args[2], &args[3]);
+
     } else {
         println!("unknown command: {}", args[1])
     }
